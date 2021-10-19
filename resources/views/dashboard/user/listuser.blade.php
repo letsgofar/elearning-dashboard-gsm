1f4842aa7cb853de526544dec218f9a79fa1bfee
Muhammad Ghifari <senseimg70@gmail.com>
2021-10-19 19:42:39 +0700

    Update CRUD di list User
    Perbaikan tombol delete

--- resources/views/dashboard/user/listuser.blade.php
+++ resources/views/dashboard/user/listuser.blade.php
@@ -48,8 +48,33 @@
                             <div class="text-center">
                                 <a href="/user/{{ $user->slug }}" class="button-icon-read mr-1"><i class="fa fa-info-circle"></i></a>
                                 <a href="/user/{{ $user->slug }}/edit" class="button-icon-edit mr-1"><i class="fa fa-pencil"></i></a>
-                                <button type="button" class="button-icon-delete mr-1" data-bs-toggle="modal" data-bs-target="#defaultModal" style=""><i class="fa fa-trash"></i></button>
-                            </div>
+                                <button type="button" class="button-icon-delete mr-1" data-bs-toggle="modal" data-bs-target="#defaultModal_{{$user->id}} " style=""><i class="fa fa-trash"></i></button>
+               <!-- Modal -->
+								<div class="modal fade" id="defaultModal_{{$user->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+									<div class="modal-dialog" role="document">
+										<div class="modal-content">
+											<div class="modal-header">
+											<h5 class="modal-title" id="exampleModalLabel">Delete User</h5>
+											<button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+												<span aria-hidden="true">×</span>
+											</button>
+											</div>
+											<div class="modal-body">
+											<p class="text-center">Apakah anda yakin ingin menghapus <br> <b> {{ $user->name }} </b> ?</p>
+											</div>
+											<div class="modal-footer">
+											<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+											<form class="d-inline" action="/user/{{ $user->slug }}/delete" method="POST">
+												@method('delete')
+												@csrf
+												<button type="submit" class="btn btn-danger">Delete</button>
+											</form>
+											</div>
+										</div>
+									</div>
+								</div>
+                <!-- Modal -->
+							</div>
                         </td>
                         </tr>
                         @endforeach
@@ -57,31 +82,6 @@
                     </table>
                 </div>
 
-                <!-- Modal -->
-                <div class="modal fade" id="defaultModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Delete User</h5>
-                            <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">×</span>
-                            </button>
-                            </div>
-                            <div class="modal-body">
-                            <p class="text-center">Apakah anda yakin ingin menghapus <br> <b> {{ $user->name }} </b> ?</p>
-                            </div>
-                            <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <form class="d-inline" action="/user/{{ $user->slug }}/delete" method="POST">
-                                @method('delete')
-                                @csrf
-                                <button type="submit" class="btn btn-danger">Delete</button>
-                            </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- Modal -->
 
                 <div class="mt-4 d-flex justify-content-center">
                     {{ $users->links() }}
