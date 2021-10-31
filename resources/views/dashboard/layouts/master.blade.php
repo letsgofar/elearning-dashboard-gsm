e80a49635498e61d5366dd936f03105856111f83
Muhammad Ghifari <senseimg70@gmail.com>
2021-10-31 08:26:27 +0700

    Perbaikan logo dan akses
    Perbaikan ukuran logo dan setting akses

--- resources/views/dashboard/layouts/master.blade.php
+++ resources/views/dashboard/layouts/master.blade.php
@@ -125,8 +125,8 @@
         <!-- partial:partials/_navbar.html -->
         <nav id="top-nav" class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
         <div id="logoweb" class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center" data-intro="Selamat datang di website Dashboard Admin GSM, Yuk kita tur dulu!" data-position="bottom" >
-            <a class="navbar-brand brand-logo mr-5" href="index.html"><img src="{{asset('assets/images/main-icon2.png')}}" class="mr-2" alt="logo"/></a>
-            <a class="navbar-brand brand-logo-mini" href="index.html"><img src="{{asset('assets/images/small-icon.png')}}" alt="logo"/></a>
+            <a class="navbar-brand brand-logo mr-5" href="/"><img src="{{asset('assets/images/main-icon2.png')}}" class="mr-2" alt="logo"/></a>
+            <a class="navbar-brand brand-logo-mini" href="/"><img src="{{asset('assets/images/small-icon.png')}}" alt="logo"/></a>
         </div>
         <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
             <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
